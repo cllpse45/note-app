44e946bddecbb214b8d08c8f009a3b513cdd3950
cllpse45 <119014171+cllpse45@users.noreply.github.com>
2023-03-16 21:19:01 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button>Create one now</button>
+
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
