9378844ff096619e2ad328de12b4bf822025118f
cllpse45 <119014171+cllpse45@users.noreply.github.com>
2023-03-26 01:07:41 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/style.css";
+import Editor from "./components/Editor";
+
 
 function App() {
+  
+  const[notes,setNotes] = React.useState([]);
+  
   return (
     <div className="App">
       <main>
-        <div className="no-notes">
+        {notes.length > 0 ? (
+          <div>{Editor}</div>
+        ): (
+          <div className="no-notes">
           <h1>You have no notes</h1>
-          <button>Create one now</button>
+          <button className="first-note">Create one now</button>
 
         </div>
+        )
+        }
       </main>
     </div>
   );
